7cd88fb9fd71499e635918e7d4847b2de5f24de7
abdikeem <abdihakimabdirahman21@gmail.com>
2022-09-10 11:31:48 +0300

    all

---
+++ index.js
@@ -0,0 +1,11 @@
+import express, { application } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`listening on port: http://localhost:${PORT}`));
